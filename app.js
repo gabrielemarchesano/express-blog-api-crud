6bedb473dfb884452071e3a459feeb3976753433
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-26 11:57:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const app = express();
+const PORT = 3000;
+
+const postsRouter = require ("./routers/posts");
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`App listening in the port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Blog routing");
+})
+
+app.use("/posts", postsRouter);
