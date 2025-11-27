eb1642dfc51c6390b0af03f3eb14c3aea9fa9a2a
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-27 13:00:44 +0100

    using the function app.use to register the body

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const postsRouter = require ("./routers/posts");
 
 app.use(express.static("public"));
 
+app.use(express.json());
+
+app.use("/posts", postsRouter);
+
+
 app.listen(PORT, () => {
   console.log(`App listening in the port ${PORT}`);
 });
@@ -13,5 +18,3 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Blog routing");
 })
-
-app.use("/posts", postsRouter);
