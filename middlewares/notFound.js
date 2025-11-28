d4b568de01a163c4dd30c2d1007bd682b6794592
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-28 11:38:33 +0100

    create middleware function to manage unexisting route

---
+++ middlewares/notFound.js
@@ -0,0 +1,9 @@
+function  notFound(req, res, next){
+  res.status(404);
+  res.json({
+    error: "Not found",
+    message: "Cannot found the path you're searching"
+  });
+};
+
+module.exports = notFound;
